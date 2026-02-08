36a54b90697818247987381af084e4023d81e799
Githubuyir <stevenabr2702@gmail.com>
2026-02-08 22:21:36 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hrms-backend-00z9.onrender.com/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // ================= EMPLOYEES =================
